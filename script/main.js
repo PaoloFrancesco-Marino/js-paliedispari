04360cb948091d4276b5b9c95015f14facde8a93
Paolo Francesco Marino <paolo.francesco.marino@gmail.com>
2020-04-02 19:27:38 +0200

    Js done

--- script/main.js
+++ script/main.js
@@ -4,35 +4,35 @@
 
 // 1 Chiedere all’utente di inserire una parola
 
-var parola = prompt('inserisci una parola').toLocaleLowerCase();
-console.log(parola);
+// var parola = prompt('inserisci una parola').toLocaleLowerCase();
+// console.log(parola);
 
 
-// 2 Creare una funzione per capire se la parola inserita è palindroma
+// // 2 Creare una funzione per capire se la parola inserita è palindroma
 
-var invertiParola = reverseParola(parola);
-console.log(invertiParola);
+// var invertiParola = reverseParola(parola);
+// console.log(invertiParola);
 
 
 
-if ( parola == invertiParola) {
-    console.log('La parola è palindroma');
+// if ( parola == invertiParola) {
+//     console.log('La parola è palindroma');
     
-} else {
-    console.log('la parola non è palindroma');
+// } else {
+//     console.log('la parola non è palindroma');
     
-}
+// }
 
 
-function reverseParola (word) {
-    var reverse = '';
+// function reverseParola (word) {
+//     var reverse = '';
 
-    for (var i = word.length -1; i >= 0; i--) {
-        reverse += word[i];
-    }
+//     for (var i = word.length -1; i >= 0; i--) {
+//         reverse += word[i];
+//     }
 
-    return reverse;
-}
+//     return reverse;
+// }
 
 
 
@@ -40,45 +40,45 @@ function reverseParola (word) {
  *  Pari e dipari
  */
 
-// 1 L’utente sceglie pari o dispari e un numero da 1 a 5. 
+// 1 L’utente sceglie pari o dispari e poi sceglie un numero da 1 a 5. 
+var scelta = prompt('Pari o Dispari?');
+var numeroUtente = parseInt(prompt('inserici un numero da 1 a 5'));
+console.log('Utente sceglie: ' + scelta);
+console.log('Il numero scelto dall\'utente: ' + numeroUtente);
 
-// var scelta = prompt('Pari o Dispari?');
+// 2 Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 
-// var numeroUtente = parseInt(prompt('inserici un numero da 1 a 5'));
-
-// // 2 Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-
-//  var numberPC = getRandomNumber(1,5);
-
-//  console.log(numberPC);
+ var numberPC = getRandomNumber(1,5);
+ console.log('Numero generato dal computer: ' + numberPC);
  
-// function getRandomNumber (min, max) { 
-//     var random = Math.floor(Math.random() * (max - min +1) ) + min;
-//     return random;
+// 3 Sommiamo i due numeri
+
+var somma = numeroUtente + numberPC;
+console.log('la somma dei numeri è: ' + somma);
+
+// 4 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+var check = pariDispari(somma);
+console.log('la somma è un numero: ' + check);
+
+// 5 Dichiariamo chi ha vinto.
+if ( check == scelta) {
+    console.log('Hai Vinto');
     
-// }
+} else {
+    console.log('Hai Perso');   
+}
 
-// // 3 Sommiamo i due numeri
+// function
 
-// var somma = numeroUtente + numberPC;
-
-// console.log(somma);
-
-// // 4 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-
-// function pariDispari (numero) {
-//     if (numero % 2 == 0) {
-//         return 'pari';
-//     }
-
-//     return 'dispari';
-// }
-
-// // 5 Dichiariamo chi ha vinto.
-
-// if (pariDispari(somma) == scelta) {
-//     console.log('Hai perso');
+function getRandomNumber (min, max) { 
+    var random = Math.floor(Math.random() * (max - min +1) ) + min;
+    return random;
     
-// } else {
-//     console.log('Hai vinto');   
-// }
+}
+
+function pariDispari (numero) {
+    if (numero % 2 == 0) {
+        return 'pari';
+    }
+    return 'dispari';
+}
